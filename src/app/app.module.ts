2545091532e900785345e8f484c2b0e394046856
joshhawks1823 <jheat23@gmail.com>
2020-03-05 17:20:12 -0500

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { ContactComponent } from "./contact/contact.component";
 import { AboutComponent } from "./about/about.component";
-// import { ProductsComponent } from './products/products.component';
-
 import { ProductsComponent } from "./products/products.component";
 import { HttpClientModule } from "@angular/common/http";
 @NgModule({
@@ -17,10 +14,6 @@ import { HttpClientModule } from "@angular/common/http";
     HomeComponent,
 
     ContactComponent,
-    AboutComponent
-    // ProductsComponent
-  ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
     AboutComponent,
     ProductsComponent
   ],
